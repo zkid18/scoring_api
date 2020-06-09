48c4c4b8b84fbf48705b298f3f2ec0893281c296
Daniel Chepenko <dchepenk@yahoo-corp.co>
2020-06-09 20:24:14 +0900

    Edited store

--- store.py
+++ store.py
@@ -1,5 +1,21 @@
 import redis
+import functools
 from datetime import timedelta, datetime
+
+def try_connection(attemps):
+    def decorator(method):
+        @functools.wraps(method)
+        def connect(*args, **kwargs):
+            for _ in range(N):
+                while True:
+                    try:
+                        return method(*args, **kwargs)
+                    except redis.ConnectionError:
+                        continue
+                    break
+        return connect
+    return decorator
+
 
 class RedisStore:
     def __init__(self, host, port):
@@ -30,6 +46,8 @@ class RedisStore:
 
 
 class Store:
+    MAX_ATTEMPS = 10
+
     def __init__(self, store):
         self.storage = store
 
@@ -39,10 +57,32 @@ class Store:
     def connect(self):
         return self.storage.connect()
     
+    @try_connection(MAX_ATTEMPS)
     def get_cache(self, key):
+        '''
+        Communication with client-server cache stoarage (i.e. memcache, tarantool, redis)
+        '''
+        return self.storage.get(key)
+    
+    @try_connection(MAX_ATTEMPS)
+    def get(self, key):
+        '''
+        Communication with separate key-value stoarage (i.e nosql)
+        '''
         return self.storage.get(key)
 
+    @try_connection(MAX_ATTEMPS)
+    def set(self, key):
+        '''
+        Communication with separate key-value stoarage (i.e nosql)
+        '''
+        return self.storage.set(key, score, seconds)
+
+    @try_connection(MAX_ATTEMPS)
     def cache_set(self, key, score, seconds=60*60):
+        '''
+        Communication with client-server cache stoarage (i.e. memcache, tarantool, redis)
+        '''
         try:
             return self.storage.set(key, score, seconds)
         except:
